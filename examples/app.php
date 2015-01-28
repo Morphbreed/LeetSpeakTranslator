1331ec3d0e1ea74541a9c143a92f44cbe65dc08d
Morphbreed <morphbreed@chello.at>
2015-01-28 19:48:47 +0100

    Update app.php

--- examples/app.php
+++ examples/app.php
@@ -6,4 +6,4 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $trans = new LeetSpeakTranslator();
 var_dump($trans->translate('i am the hero and', 3));
 
-var_dump($trans->untranslate('! 4M 1H3 H320 4ND 1337'));
+var_dump($trans->revert('! 4M 1H3 H320 4ND 1337'));
